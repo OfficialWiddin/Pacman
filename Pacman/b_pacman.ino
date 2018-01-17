f3f4361a7b584454cd110758efa9d666e11f3731
Widdin <31885198+OfficialWiddin@users.noreply.github.com>
2018-01-17 16:26:57 +0100

    Updated comments.

--- Pacman/b_pacman.ino
+++ Pacman/b_pacman.ino
@@ -6,18 +6,15 @@
 #define BLUE     RGB(0, 0,   255)
 
 #include "map.h"
-  
 #include "assets.h"
 
-
-//Rotating images.
+//Function for rotating images.
 static void rotate_64_64(uint16_t a)
 {
   GD.cmd_translate(F16(8),F16(8));
   GD.cmd_rotate(a);
   GD.cmd_translate(F16(-8), F16(-8));
 }
-
 
 class Pacman {
   public:
@@ -28,34 +25,38 @@ class Pacman {
     bool move_up = true;
     bool move_down = true;
 
-    //If power up is active.
+    //Check the state of powerup.
     bool power_up = false;
 
-    //Movement speed.
+    //Movement speed of Pacman.
     int movement_speed = 2;
     
     //Movement direction.
     //1 = left, 2 = right, 3 = up, 4 = down
     int move_direction = 0;
 
-    //Pacmans score.
+    //Score.
     int score = 0;
 
-    //Start position.
+    //Pacman start position.
     int posX = 113;
     int posY = 188;
-
+    
+    //Index in the map.
     int current_index = 0;
+
+    //Total healthpoints.
     int health = 1;
 
-    //Because it renders from the top left corner.
+    //Offset because it renders from the top left corner.
     int Yoffset = 8;
     int Xoffset = 8;
     
+    //Render Pacman.
     void pac_render(float frame)
     {
-      int deg = 0;
       //Rotate image depending on direction.
+      int deg = 0;
       if(move_direction == 1)
       {
         deg = 180;
@@ -73,7 +74,7 @@ class Pacman {
         deg = 90;
       }
 
-      //Jump between two images depending on the frame, "animation".
+      //Jump between two images depending on the frame, To make an "animation".
       if(frame < 0.33)
       {
         GD.Begin(BITMAPS);
@@ -91,7 +92,6 @@ class Pacman {
         GD.Vertex2ii(posX-Xoffset,posY-Yoffset,1,0+1);
         rotate_64_64(DEGREES(-deg));
         GD.cmd_setmatrix();
-        
       }
       else if(frame > 0.66)
       {
@@ -104,15 +104,16 @@ class Pacman {
       }
     }
     
+    //Pacman movement.
     void pac_move()
     {
-      //Calculate the players index.
+      //Calculate the players index in the map.
       int xpos = (posX / 8);
       int ypos = (posY / 8);
       int index = (ypos*28 + xpos);
       current_index = index;
 
-      //Check available directions.
+      //Check available walkable directions.
       if(bot_level[index+1] == 0)
       {
         move_right = false;
@@ -130,28 +131,25 @@ class Pacman {
         move_up = false;
       }
 
-      //If we walk on a pellet.
-      if(my_level[index] == 2)
+      if(my_level[index] == 2) //If we walk on a pellet.
       {
         my_level[index] = 1;
         score += 10;
       }
-      //If we walk on a powerup.
-      else if(my_level[index] == 3)
+      else if(my_level[index] == 3) //If we walk on a powerup.
       {
         power_up = true;
         GD.play(PIANO,60);
         my_level[index] = 1;
       }
-      //If we walk on a teleport.
-      else if(my_level[index] == 5)
+      else if(my_level[index] == 5) //If we walk on a teleport.
       {
-        if(index == 419) //Right to Left side.
+        if(index == 419) //Teleport from right to left side.
         {
           posX = 10;
           posY = 117;
         }
-        else if(index == 392) //Left to Right side.
+        else if(index == 392) //Teleport from left to right side.
         {
           posX = 215;
           posY = 117;
@@ -194,7 +192,7 @@ class Pacman {
         posY += movement_speed;
       }
 
-      //Reset
+      //Reset the movement.
       move_right = true;
       move_left = true;
       move_up = true;
